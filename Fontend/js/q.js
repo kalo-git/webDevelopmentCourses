4c562f156f884ea976c6e691a5c14dd04672db54
Ramesh Roy <rameshroyprl2019@gmail.com>
2025-01-21 21:20:48 +0530

    good night

--- Fontend/js/q.js
+++ Fontend/js/q.js
@@ -45,10 +45,99 @@ Write a function `concatenateStrings(str1, str2)` that takes two strings `str1` 
 Constraints:
 `1 ≤ str1.length, str2.length ≤ 100`
 */
-
+// this is function code of add two strings:
 function concatenateStrings(str1, str2) {
     return str1 + str2;
 }   
 
 const result = concatenateStrings('Hello ', 'World');
 console.log("this is function code of concatenate two strings:", result, "\n");
+
+// normal code of add two strings:
+let str3 = 'giridhari ';
+let str4 = 'karmakar';
+
+const myFullName = str3 + str4;
+
+console.log("My full Name is ", myFullName, "\n");
+
+// 3. Mixed Addition
+
+/* Problem Statement: 
+
+Write a function `mixedAddition(a, b)` that takes two inputs, where one is a string and the other is a number. Return the result of adding them.  
+
+Constraints:  
+
+- `a` or `b` will always be a string, the other will always be a number.  
+- `1 ≤ |a| ≤ 100`  
+- `-10^3 ≤ b ≤ 10^3`
+
+--- */
+
+
+function mixedAddition (a,b){
+    a = "giri ";
+    b = 7577
+    sum = a+b
+
+    return sum;
+
+}
+ const sumOf = mixedAddition ();
+
+ console.log(sumOf, "\n")
+
+
+//  4. Sum of Array Elements
+/*
+Problem Statement:
+
+Write a function `sumArray(nums)` that takes an array of numbers and returns their sum.  
+
+Constraints:
+
+- `1 ≤ nums.length ≤ 10^3`  
+- `-10^3 ≤ nums[i] ≤ 10^3`
+*/
+
+function sumArray (nums){
+    total = 0
+     for (let num of nums){
+        total += num
+
+     }
+return total;
+
+
+}
+console.log(sumArray([1, 2, 3, 4, 5])); // Output: 15
+console.log(sumArray([-1, -2, -3, 4, 5])); // Output: 3
+console.log(sumArray([])); 
+
+// 5. String to Integer Addition 
+
+/*
+problem Statement:
+
+Write a function `addStringAndNumber(str, num)` that converts the string `str` to an integer and adds it to the number `num`.  
+
+Constraints:
+
+- `str` will always represent a valid integer.  
+- `-10^3 ≤ int(str), num ≤ 10^3`
+*/
+
+
+
+function addStringAndNumber (str,num){
+ str = "giri "
+ num = 12345
+ sumOfadd = str + num
+
+ return sumOfadd;
+
+}
+const addMy = addStringAndNumber ();
+
+console.log (addMy, "\n")
