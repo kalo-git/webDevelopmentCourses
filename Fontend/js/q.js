9f4e4994c0a8d2c29167286200c6c3127f62342a
Ramesh Roy <rameshroyprl2019@gmail.com>
2025-01-21 20:47:33 +0530

    add

--- Fontend/js/q.js
+++ Fontend/js/q.js
@@ -1,3 +1,4 @@
+
 // 1 Add Two Integers
 
 /*
@@ -8,6 +9,9 @@ Constraints:
 -10^3 ≤ a, b ≤ 10^3
 */
 
+console.log("1 Add Two Integers\n");
+
+
 // this is normal code of add two numbers:
 
 let a = 10;
@@ -15,7 +19,10 @@ let b = 5;
 
 let sum = a + b;
 
-console.log('a + b is ' , sum);
+
+console.log("this is normal code of add two numbers:");
+console.log('a + b is ' , sum, "\n");
+
 
 // this is function sum of add two numbers :
 
@@ -25,7 +32,23 @@ function addTwoNumbers(a, b) {
 
 const reajel = addTwoNumbers (10, 5); 
 
-console.log('Tha Sum of two number is ', reajel );
+console.log("this is function code of add two numbers:");
+console.log('Tha Sum of two number is ', reajel, "\n" );
 
 
 //2. Concatenate Strings
+
+console.log("2. Concatenate Strings\n");
+/*
+Write a function `concatenateStrings(str1, str2)` that takes two strings `str1` and `str2` and returns their concatenation.  
+
+Constraints:
+`1 ≤ str1.length, str2.length ≤ 100`
+*/
+
+function concatenateStrings(str1, str2) {
+    return str1 + str2;
+}   
+
+const result = concatenateStrings('Hello ', 'World');
+console.log("this is function code of concatenate two strings:", result, "\n");
